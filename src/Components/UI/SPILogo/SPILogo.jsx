5127325e9459538e36c0406369bbe2869cbbf941
rashidulislamg71 <jrashidulislam0103@gmail.com>
2025-11-09 19:54:00 +0600

    logo path remove and use direct

--- src/Components/UI/SPILogo/SPILogo.jsx
+++ src/Components/UI/SPILogo/SPILogo.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import spi_logo from "/images/SPI_Logo.png";
+import React from "react";
 import styles from "./SPILogo.module.css";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const SPILogo = () => {
-    return (
-        <Link to="/" className="flex items-center gap-1">
-            <img className={`${styles.logo}`} src={spi_logo} alt="Square Polytechnic institute Logo" />
-            <h3><b className={`${styles.logo_text}`} >স্কয়ার পলিটেকনিক  <br /> ইন্সটিটিউট</b></h3>
-        </Link>
-    );
+  return (
+    <Link to="/" className="flex items-center gap-1">
+      <img
+        className={`${styles.logo}`}
+        src="/images/SPI_Logo.png"
+        alt="Square Polytechnic institute Logo"
+      />
+      <h3>
+        <b className={`${styles.logo_text}`}>
+          স্কয়ার পলিটেকনিক <br /> ইন্সটিটিউট
+        </b>
+      </h3>
+    </Link>
+  );
 };
 
 export default SPILogo;
